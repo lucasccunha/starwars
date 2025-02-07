86759522c8684a7b1d09cbdfc2e88e63200035f0
Lucas Cunha <lucas71cunha@gmail.com>
2025-02-06 21:20:01 -0300

    first commit

---
+++ src/main/java/com/example/sw_planet_api/web/PlanetController.java
@@ -0,0 +1,27 @@
+package com.example.sw_planet_api.web;
+
+import com.example.sw_planet_api.domain.Planet;
+import com.example.sw_planet_api.domain.PlanetService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/planets")
+public class PlanetController {
+
+    private PlanetService planetService;
+
+    public PlanetController(PlanetService planetService) {
+        this.planetService = planetService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Planet> create(@RequestBody Planet planet) {
+        Planet planetCreated = planetService.create(planet);
+        return ResponseEntity.status(HttpStatus.CREATED).body(planetCreated);
+    }
+}
